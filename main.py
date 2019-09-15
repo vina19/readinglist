3542862721d8a795ee6daad8bbab74d77504eae1
Dani <dani@dani.dani>
2019-09-14 21:07:46 -0500

    Prints a message if user tries to modify a book that is not in database

--- main.py
+++ main.py
@@ -63,12 +63,14 @@ def search_book():
 
 
 def change_read():
-
-    book_id = ui.get_book_id()
-    book = store.get_book_by_id(book_id)  
-    new_read = ui.get_read_value()     
-    book.read = new_read 
-    book.save()
+    try:
+        book_id = ui.get_book_id()
+        book = store.get_book_by_id(book_id)  
+        new_read = ui.get_read_value()     
+        book.read = new_read 
+        book.save()
+    except:
+        print('That book is not in the database!')
     
 
 def quit_program():
